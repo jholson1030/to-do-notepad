913a726e1a317951d3f347819a652d89d358a20d
John Olson <jholson1030@gmail.com>
2023-09-17 23:54:27 -0500

    Set variables to all page elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set variables to all page elements
+
+const noteContainer = document.querySelector('#note-container');
+const noteInput = document.querySelector('#note-input');
+const noteSubmit = document.querySelector('#note-submit');
