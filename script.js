fc54cf8b3caab8de944ad1da2cc5bf1146b69bfb
John Olson <jholson1030@gmail.com>
2023-09-18 00:00:12 -0500

    Comment out code

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 const noteContainer = document.querySelector('#note-container');
 const noteInput = document.querySelector('#note-input');
 const noteSubmit = document.querySelector('#note-submit');
+
+// Function that will take typed notes, and place them in noteContainer
+
