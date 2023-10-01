93d7238385d9d6f2855f8c3b908bec5c8dd934ea
John Olson <jholson1030@gmail.com>
2023-09-30 21:27:37 -0500

    Declare noteTasks as an empty array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Set variables to all page elements
 
 const noteContainer = document.querySelector('#note-container');
+let noteTasks = [];
 const noteInput = document.querySelector('#note-input');
 const noteSubmit = document.querySelector('#note-submit');
 
 // Function that will take typed notes, and place them in noteContainer
-
