eafcdd49f68cf699a4d3a839eacdc4aa049b9aa9
John Olson <jholson1030@gmail.com>
2023-10-02 22:33:24 -0500

    Add submit event listener

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Set variables to all page elements
 
 const noteContainer = document.querySelector('#note-container');
-let noteTasks = [];
 const noteInput = document.querySelector('#note-input');
 const noteSubmit = document.querySelector('#note-submit');
 
-// Function that will take typed notes, and place them in noteContainer
+// Note array
+let noteTasks = [];
+
+// Listening for a button click
+noteSubmit.addEventListener('click', function() {
+    // Grab value from the input
+    const noteInputValue = noteInput.value.trim();
+
+    // Checks if the input is not empty
+    if (noteInputValue.length) {
+        // Add the task to the task array
+        noteTasks.push(noteInputValue);
+
+        // Update the DOM
+        updateTasks();
+
+        // Clear the input field
+        noteInput.value = '';
+    }
+});
